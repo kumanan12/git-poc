c786d4afaea5601ddf01bad554759d21823bb393
Kumanan Murugesan <kumanan12@gmail.com>
2023-10-09 09:19:23 -0400

    Worked on creating layouts

---
+++ scripts/scope-demo.js
@@ -0,0 +1,19 @@
+var publicName = 'Anyone can see me';
+
+function greeting(name) {
+    console.log(publicName);
+    if (name === 'kumanan') {
+        // block level scope
+        let specialGreeting = 'Mr.'   
+    }
+    // function level scope
+    let welcome = ' Hello ';
+    console.log(welcome);
+    return specialGreeting + welcome + name; 
+
+}
+
+
+//  console.log('current dir ' + __filename);
+const result = greeting('kumanan');
+console.log(result);
